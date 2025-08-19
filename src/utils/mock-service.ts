1117fef830ddeb1f23bee1f63a547bfda3544303
Mohd Ahsan <63741546+mofahsan@users.noreply.github.com>
2025-08-19 18:45:36 +0530

    add empty json path

--- src/utils/mock-service.ts
+++ src/utils/mock-service.ts
@@ -8,15 +8,7 @@ export async function callMockService(domain:any,submissionData:any){
     // const submissionUrl = `${process.env.MOCK_SERVICE_URL?.replace("domain",domain)}/flow/new`
     const submissionUrl = `${process.env.BACKEND_URL}/flow/proceed`
     
-    const mockSubmitData = {...submissionData, inputs:{}}
-//     const mockSubmitData = {...submissionData, 
-//               "json_path_changes": {
-//     "$.message.intent.fulfillment.stops[?(@.type=='START')].location.city.code": "std:080"
-//   },
-//   "inputs": {
-//     "city_code": "std:080"
-//   }
-//     }
+    const mockSubmitData = {...submissionData, inputs:{}, json_path_changes:{}}
 
 
     const result =  await axios.post(submissionUrl,mockSubmitData)
